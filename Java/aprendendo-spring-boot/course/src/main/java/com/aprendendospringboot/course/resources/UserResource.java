6419f139cf4ad42aa38e06595d25c5b127d54fa3
OscarSilvaOfficial <oscarkaka222@gmail.com>
2020-09-28 18:32:19 -0300

    Add classes User

---
+++ Java/aprendendo-spring-boot/course/src/main/java/com/aprendendospringboot/course/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.aprendendospringboot.course.resources;
+
+import org.apache.catalina.connector.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.aprendendospringboot.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Oscar", "oscar@gmail.com", "9999999", "123456");
+		return ResponseEntity.ok().body(u);
+	}
+}
