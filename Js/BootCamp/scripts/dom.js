d13306fb59967f8c5676e9dc108f271893719aff
OscarSilvaOfficial <oscarkaka222@gmail.com>
2020-10-05 18:28:18 -0300

    Adicionando icone do GitHub

--- Js/BootCamp/scripts/dom.js
+++ Js/BootCamp/scripts/dom.js
@@ -1,7 +1,7 @@
-let ul = document.querySelector(".container ul")
-let input = document.querySelector(".container input")
-let button = document.querySelector(".container #button")
-let removeButton = document.querySelector(".container ul li .btn btn-primary")
+let ul = document.querySelector(".container ul");
+let input = document.querySelector(".container input");
+let button = document.querySelector(".container #button");
+let removeButton = document.querySelector(".container ul li .btn btn-primary");
 
 let todos = [
     
@@ -20,8 +20,8 @@ function renderList() {
         let ind = todos.indexOf(todo);
 
         li.appendChild(todoText);
-        li.setAttribute('class', 'list-group-item')
-        li.style.marginBottom = '.5em'
+        li.setAttribute('class', 'list-group-item');
+        li.style.marginBottom = '.5em';
         ul.appendChild(li);
         linkRemove.setAttribute('href', '#');
         linkRemove.setAttribute('onclick', 'delList(' + ind + ')');
@@ -33,32 +33,36 @@ function renderList() {
 }
 
 function addList() {
-    let textInput = input.value
+    let textInput = input.value;
 
     todos.push(textInput);
     input.value = '';
-    renderList()
+    renderList();
 }
 
 function delList(ind){
-    todos.splice(ind, 1)
-    renderList()
+    todos.splice(ind, 1);
+    renderList();
 }
 
 function git () {
 
     let linkElement = document.createElement('a');
-    let textElement = document.createTextNode('Acessar meu GitHub');
+    let git = document.createElement('i');
     let container = document.querySelector('.container');
     let div = document.createElement("div")
 
     linkElement.setAttribute('href', 'https://github.com/OscarSilvaOfficial');
-    linkElement.appendChild(textElement);
+    linkElement.appendChild(git);
+    linkElement.setAttribute('target', '_blank');
     container.style.textAlign = 'center';
-    container.appendChild(div)
-    div.appendChild(linkElement)
+    container.appendChild(div);
+    git.style.fontSize = '2em';
+    git.style.color = 'black';
+    git.setAttribute('class', 'fa fa-github');
+    div.appendChild(linkElement);
 
 }
 
-git()
+git();
 button.onclick = addList;
