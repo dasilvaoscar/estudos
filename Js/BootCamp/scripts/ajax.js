23c8926217a6c2322818890e6d67db5a9f997fa1
OscarSilvaOfficial <oscarkaka222@gmail.com>
2020-10-05 21:56:00 -0300

    Acessando a API Github

--- Js/BootCamp/scripts/ajax.js
+++ Js/BootCamp/scripts/ajax.js
@@ -1,10 +1,7 @@
-let ajax = new XMLHttpRequest;
-
-ajax.open('GET', 'https://api.github.com/users/OscarSilvaOfficial');
-ajax.send();
-
-ajax.onreadystatechange = () => {
-    if (ajax.readyState === 4){
-        console.log(JSON.parse(ajax.responseText));
-    }
-}
+axios.get('https://api.github.com/users/OscarSilvaOfficial')
+    .then(function(response){
+        console.log(response.config.url);
+    })
+    .catch(function(error){
+        console.warn(error);
+    });
