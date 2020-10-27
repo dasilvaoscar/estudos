2891c5df3840abbafed8a0bb476643fd0cebe16d
OscarSilvaOfficial <oscarkaka222@gmail.com>
2020-10-27 19:39:01 -0300

    Overide da função sacar para adequar taxas

--- Js/Js OO/modules/entities/ContaCorrente.js
+++ Js/Js OO/modules/entities/ContaCorrente.js
@@ -2,5 +2,12 @@
  
  export class ContaCorrente extends Conta {
 
+    sacar(valor) {
+        if(this.getSaldo() < valor) return
+        const taxa = valor * 0.1
+
+        this.saldo -= (valor + taxa)
+        console.log(`Saldo Atual: R$ ${this.getSaldo()}`)
+    }
 
 }
