d0bebf4fd8839c2829a9b0479e41aa479bdf2553
OscarSilvaOfficial <oscarkaka222@gmail.com>
2021-01-16 12:26:03 -0300

    Atualizando estudos

--- Js/introducao-javascript/js/main.js
+++ Js/introducao-javascript/js/main.js
@@ -1,2 +1,5 @@
-document.querySelector('h1').textContent = 'Oscar da Silva'
-document.querySelector('h2').textContent = 'Minha Tabela'
+let list = [1, 2, 4, 5]
+
+list.map((res, i) => {
+  i == 2 ? print('2'):
+})
