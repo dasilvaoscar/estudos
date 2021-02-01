3a8ec45d4ea4b33347017fcfefa27b588e5a29b7
OscarSilvaOfficial <oscarkaka222@gmail.com>
2021-02-01 14:12:17 -0300

    Add alteraçoes

---
+++ Python/.history/Scripts/tests_20210201140935.py
@@ -0,0 +1,3 @@
+x = {"key1": "value1 from x", "key2": "value2 from x"}
+
+print('teste')
--- Python/.history/Scripts/tests_20210201141137.py
+++ Python/.history/Scripts/tests_20210201141137.py
@@ -1,4 +1,4 @@
 x = {"key1": "value1 from x", "key2": "value2 from x"}
 y = {"key2": "value2 from y", "key3": "value3 from y"}
 
-print(x | y)
+print(x |= y)
