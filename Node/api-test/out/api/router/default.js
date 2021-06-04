93e720f380efbb08efae419a6f8e1c69cba58531
OscarSilvaOfficial <oscarkaka222@gmail.com>
2021-06-04 15:28:35 -0300

    update

--- Node/api-test/out/api/router/default.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.router = void 0;
-const express_1 = require("express");
-const index_1 = require("../controller/index");
-exports.router = express_1.Router();
-exports.router.get('/', index_1.UserController.home);
-exports.router.post('/', index_1.UserController.createUser);
-//# sourceMappingURL=default.js.map
