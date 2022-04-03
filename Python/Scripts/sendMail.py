82434d3fbe91ea98bb2981afe689e3bc6fcccfc0
OscarSilvaOfficial <OscarSilvaOfficial>
2022-04-03 12:44:59 -0300

    update

--- Python/Scripts/sendMail.py
+++ Python/Scripts/sendMail.py
@@ -1,25 +1,34 @@
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+
 mail_content = '''Hello,
 This is a simple mail. There is only text, no attachments are there The mail is sent using Python SMTP library.
 Thank You
 '''
-#The mail addresses and password
-sender_address = 'junior.devq@gmail.com'
-sender_pass = 'pass'
-receiver_address = 'oscarkaka222@gmail.com'
-#Setup the MIME
-message = MIMEMultipart()
-message['From'] = sender_address
-message['To'] = receiver_address
-message['Subject'] = 'A test mail sent by Python. It has an attachment.'   #The subject line
-#The body and the attachments for the mail
-message.attach(MIMEText(mail_content, 'plain'))
-#Create SMTP session for sending the mail
-session = smtplib.SMTP('smtp.gmail.com', 587) #use gmail with port
-session.starttls() #enable security
-session.login(sender_address, sender_pass) #login with mail_id and password
-text = message.as_string()
-session.sendmail(sender_address, receiver_address, text)
-session.quit()
+
+class EmailSender():
+  
+  def __init__(self):
+    self.__sender_address = 'oscarkaka222@gmail.com'
+    self.__sender_pass = 'senha_de_app'
+    self.__receiver_address = 'oscarkaka222@gmail.com'
+    
+  def __load_email_content(self):
+    message = MIMEMultipart()
+    message['From'] = self.__sender_address
+    message['To'] = self.__receiver_address
+    message['Subject'] = 'A test mail sent by Python. It has an attachment.'  
+    message.attach(MIMEText(mail_content, 'plain'))
+    return message.as_string()
+
+  def send(self):
+    session = smtplib.SMTP('smtp.gmail.com', 587) 
+    session.starttls()
+    session.login(self.__sender_address, self.__sender_pass)
+    text = self.__load_email_content()
+    session.sendmail(self.__sender_address, self.__receiver_address, text)
+    session.quit()
+
+email_sender = EmailSender()
+email_sender.send()
