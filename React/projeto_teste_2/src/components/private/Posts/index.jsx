4383539011378fe13297dcfc5899c7fefa92b33f
OscarSilvaOfficial <OscarSilvaOfficial>
2022-05-31 22:41:24 -0300

    Melhoria no código

--- React/projeto_teste_2/src/components/private/Posts/index.jsx
+++ React/projeto_teste_2/src/components/private/Posts/index.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import { DefaultCard as Card } from '../../public/Cards/Default/index';
 import { DefaultInput as Input } from '../../../components/public/inputs/Default/index'
 import * as API from '../../../services/posts/index';
@@ -17,24 +17,24 @@ export function Posts() {
     return response.json();
   };
 
-  const mountAllPosts = async () => {
+  const mountAllPosts = useCallback(async () => {
     const [ photos, allPosts ] = await Promise.all([fetchPhotos(), fetchPosts()]);
 
-    const postWithPhotos = allPosts.map((post) => ({
+    const postWithPhotos = allPosts.map(post => ({
       ...post,
-      image: photos.find((photo) => photo.id === post.id).url,
+      image: photos.find(photo => photo.id === post.id).url,
     }));
 
-    [setPosts, setAllPosts].map((setter) => setter(postWithPhotos));
-  };
+    [setPosts, setAllPosts].map(setter => setter(postWithPhotos));
+  }, []);
 
   const filterPosts = ({ target: { value: title }}) => {
-    const filteredPosts = allPosts.filter((post) => post.title.toUpperCase().includes(title.toUpperCase()));
+    const filteredPosts = allPosts.filter(post => post.title.toUpperCase().includes(title.toUpperCase()));
     setPosts(filteredPosts);
   }
 
   const renderPosts = () => {
-    return posts.map((post) => (
+    return posts.map(post => (
       <Card
         key={post.id}
         title={post.title}
@@ -44,9 +44,7 @@ export function Posts() {
     ))
   }
 
-  useEffect(() => {
-    mountAllPosts();
-  });
+  useEffect(() => { mountAllPosts() }, [mountAllPosts]);
 
   return (
     <section className="container">
