873dd2d1d7edc4f07eb50fb34660044cdd345163
OscarSilvaOfficial <OscarSilvaOfficial>
2022-05-31 23:49:00 -0300

    update

---
+++ React/projeto_teste_hooks/src/App.js
@@ -0,0 +1,31 @@
+import * as React from 'react';
+
+function App() {
+  const [count, setCount] = React.useState(0);
+
+  React.useEffect(() => {
+    const message = 'Sempre executa quando o componente é atualizado';
+    console.log(message)
+  });
+
+  React.useEffect(() => {
+    const message = 'Sempre que o componente for montado, o useEffect será executado';
+    console.log(message)
+  }, []);
+
+  React.useEffect(() => {
+    const message = 'Sempre que a variável count for alterada, o useEffect será executado novamente';
+    console.log(message + ' ' + count);
+  }, [count]);
+
+  return (
+    <div className="App" style={{ textAlign: 'center' }}>
+      <h1>Contador: {count}</h1>
+      <button type='button' onClick={() => setCount(count + 1)}>
+        +  
+      </button>
+    </div>
+  );
+}
+
+export default App;
