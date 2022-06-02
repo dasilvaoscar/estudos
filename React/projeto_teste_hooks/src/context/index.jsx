fb9af81a27eba2cd700adab6669e9a3fe7b08807
OscarSilvaOfficial <OscarSilvaOfficial>
2022-06-02 10:23:30 -0300

    Correção no nome da var

--- React/projeto_teste_hooks/src/context/index.jsx
+++ React/projeto_teste_hooks/src/context/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export const Context = React.createContext();
+export const GlobalContext = React.createContext();
 
 export const AppContext = ({ children }) => {
   const [counter, setCounter] = React.useState(0);
   const counterState = { counter, setCounter }
   const state = { counterState }
-  return <Context.Provider  value={ state }>{ children }</Context.Provider>
+  return <GlobalContext.Provider  value={ state }>{ children }</GlobalContext.Provider>
 }
