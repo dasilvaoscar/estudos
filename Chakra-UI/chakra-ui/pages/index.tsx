c31b697c0ca08fc6ad88c83ed10ac7a9ae9482de
OscarSilvaOfficial <OscarSilvaOfficial>
2022-06-06 22:52:51 -0300

    update

---
+++ Chakra-UI/chakra-ui/pages/index.tsx
@@ -0,0 +1,14 @@
+import { TestGeral } from "./components/TestGeral"
+import { TestGrid } from './components/TestGrid'
+
+
+const Home = () => {
+  return (
+    <>
+      <TestGeral />
+      <TestGrid />
+    </>
+  )
+}
+
+export default Home
